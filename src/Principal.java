b19271e431eb3a831756b53e27fb7629faac4b62
Felipe Johnny <felipijohnny@outlook.com>
2023-10-04 13:31:45 -0300

    aplicando Herança nas class existentes

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,5 @@
 import br.com.felipejohnny.screenmatch.modelos.Filme;
+import br.com.felipejohnny.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -8,12 +9,25 @@ public class Principal {
         filme1.setAnoDeLancamento(2020);
         filme1.setDuracaoEmMinutos(310);
         filme1.setIncluidoNoPlano(true);
+        filme1.setDiretor("Alguem incrivel");
 
         filme1.exibeFixaTecnica();
         filme1.avalia(10);
         filme1.avalia(5);
         System.out.println("A media das avaliações são: " + filme1.PegaMedia());
         System.out.println("total de avaliações: " + filme1.getTotalDeAvaliacoes());
+        System.out.println("Diretor: "+ filme1.getDiretor());
+
+
+        Serie serie = new Serie();
+
+        serie.setNome("One Piece");
+        serie.setAnoDeLancamento(2023);
+        serie.exibeFixaTecnica();
+        serie.setTemporadas(1);
+        serie.setEpisodiosPorTemporadas(12);
+        serie.setMinutosPorEpisodio(110);
+
 
     }
 }
